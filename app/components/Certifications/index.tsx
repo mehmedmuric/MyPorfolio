daeabd7e9eb3e9538db566683aa29c518f6adb53
Mehmed Muric <mehmedmuric22@gmail.com>
2025-10-23 05:33:36 +0200

    certifications\

--- app/components/Certifications/index.tsx
+++ app/components/Certifications/index.tsx
@@ -7,16 +7,12 @@ const Certifications = () => {
   return (
     <section className="pt-16">
       <div className="container">
-      <SectionTitle
-          title="Certifications"
-          paragraph=""
-          center
-          mb="50px"
-        />
-        <div className="-mx-4 flex flex-wrap">
+        <SectionTitle title="Certifications" paragraph="" center mb="50px" />
+
+        <div className="-mx-4 flex flex-wrap justify-center">
           <div className="w-full px-4">
             <div
-              className="wow fadeInUp bg-gray-dark flex flex-wrap items-center justify-center rounded-sm px-8 py-8 sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]"
+              className="wow fadeInUp bg-gray-dark flex flex-wrap justify-center gap-10 rounded-sm px-6 py-10 sm:px-10 md:px-[60px] md:py-[50px] xl:p-[60px] 2xl:px-[80px] 2xl:py-[70px]"
               data-wow-delay=".1s"
             >
               {brandsData.map((brand) => (
@@ -36,18 +32,24 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
   const { href, image, name } = brand;
 
   return (
-    <div className="mx-3 flex w-full max-w-[160px] items-center justify-center py-[15px] sm:mx-4 lg:max-w-[130px] xl:mx-6 xl:max-w-[150px] 2xl:mx-8 2xl:max-w-[160px]">
+    <div className="flex w-full flex-col items-center justify-center sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
       <a
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-12 w-full opacity-70 grayscale transition hover:opacity-100 hover:grayscale-0 dark:opacity-60 dark:hover:opacity-100"
+        className="relative flex h-32 w-32 items-center justify-center opacity-70 grayscale transition hover:opacity-100 hover:grayscale-0 dark:opacity-70 dark:hover:opacity-100"
       >
-        <Image src={image} alt={name} width={500} height={500} priority sizes="(max-width: 640px) 100vw,  
-                  (max-width: 1024px) 80vw,  
-                  (max-width: 1280px) 70vw,  
-                  50vw"/>
+        <Image
+          src={image}
+          alt={name}
+          width={128}
+          height={128}
+          className="object-contain"
+          priority
+          sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 33vw"
+        />
       </a>
+      <p className="mt-3 text-sm text-mygreen/80 font-bold text-center">{name}</p>
     </div>
   );
 };
